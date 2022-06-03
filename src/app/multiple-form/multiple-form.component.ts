6c2d7f23e7a65578e2937abd6ccbbe780e63a502
SnehaMistry <snehalad98@gmail.com>
2022-06-03 19:52:29 +0530

    design the form and progress

---
+++ src/app/multiple-form/multiple-form.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-multiple-form',
+  templateUrl: './multiple-form.component.html',
+  styleUrls: ['./multiple-form.component.scss']
+})
+export class MultipleFormComponent implements OnInit {
+  years: number[];
+  makers: string[];
+  model: string[];
+  step : number;
+  constructor() {
+    this.years = [];
+    this.makers = ['Toyota', 'Volkswagen', 'Tesla', 'BMW', 'Ferrari', 'Ford', 'Porsche', 'Honda', 'Lamborghini', 'Toyota', 'Bentley', 'Maserati'];
+    this.model = ['MG5 EV Long Range', 'NEW MG ZS EV', 'MG HS Plug-in Hybrid', 'Tesla Model S', 'Tesla Model X', 'Tesla Cybertruck', 'Tesla Model Y', 'Tesla Model 3', 'Tata Bolt', 'Tata Vista', 'Tata Xenon', 'Tata Tigor', 'Tata Magic', 'Tata Safari'];
+    for (let i = new Date().getFullYear(); i > 1990; --i) {
+      this.years.push(i);
+    }
+
+    this.step = 0;
+  }
+
+  ngOnInit(): void {
+  }
+
+  public back(){
+    this.step -= 1;
+  }
+
+  public continue(){
+    this.step += 1;
+  }
+
+}
